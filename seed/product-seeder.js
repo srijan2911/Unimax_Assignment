c2fa6e2520fa84abdabe3f47dd5df98c5d51cc50
Srijan <srijankarn2911@gmail.com>
2020-05-31 10:30:32 +0530

    did some changes

--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -7,28 +7,28 @@ mongoose.connect('localhost:27017/shopping');
 //done the same thing in array so that we can use loop.
 var products = [
     new Product({
-        imagePath: 'https://rukminim1.flixcart.com/image/352/352/j65cnm80/flour/z/s/r/1-superior-mp-atta-whole-wheat-flour-aashirvaad-original-imaewzbkfqhy4dhq.jpeg?q=70',
-        title: 'Fresh Chakki aata',
-        description: '1kg aata',
-        price: 32
+        imagePath: 'https://www.facebook.com/ChennaiBankAuctionProperties/?ref=py_c',
+        title: 'Chennai Bank Auction',
+        description: 'Auction Property in Chennai',
+        price: 1000000
     }),
     new Product({
-        imagePath: 'https://images-na.ssl-images-amazon.com/images/I/61ZhyN3P4tL._SX425_.jpg',
-        title: 'Maida',
-        description: '1kg maida',
-        price: 30
+        imagePath: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepressjournal.in%2Fmumbai%2Fbmc-to-auction-properties-to-recover-property-tax-dues&psig=AOvVaw0IhxtSO0K0LjN95CRPWFhq&ust=1590996033623000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNj31cP-3OkCFQAAAAAdAAAAABAJ',
+        title: 'BMC Auction Property',
+        description: 'BMC to auction properties to recover property tax dues',
+        price: 5000000
     }),
     new Product({
-        imagePath: 'https://cdn.grofers.com/app/images/products/full_screen/pro_52.jpg',
-        title: 'Soyabean oil',
-        description: '1L soyabean oil',
-        price: 45
+        imagePath: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fm.economictimes.com%2Fnews%2Fpolitics-and-nation%2Fgovernment-plans-to-auction-enemy-properties-worth-rs-1-lakh-crore%2Farticleshow%2F62494325.cms&psig=AOvVaw0IhxtSO0K0LjN95CRPWFhq&ust=1590996033623000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNj31cP-3OkCFQAAAAAdAAAAABAQ',
+        title: 'Enemy property auction',
+        description: 'Government plans to auction enemy properties worth Rs 1 lakh crore',
+        price: 100000000000
     }),
     new Product({
-        imagePath: 'https://images-na.ssl-images-amazon.com/images/I/61SacrIvdEL._SL1000_.jpg',
-        title: 'Salt',
-        description: '1kg tata salt',
-        price: 18
+        imagePath: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.zeebiz.com%2Findia%2Fnews-buy-your-dream-house-sbi-mega-e-auction-nears-over-1000-residential-commercial-properties-on-offer-all-details-here-86857&psig=AOvVaw0IhxtSO0K0LjN95CRPWFhq&ust=1590996033623000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNj31cP-3OkCFQAAAAAdAAAAABAW',
+        title: 'Buy your dream house',
+        description: 'Buy your dream house! SBI Mega E-Auction nears, over 1000 residential, commercial properties on offer - all details here',
+        price: 70000000
     })
 
 ];
